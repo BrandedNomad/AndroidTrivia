f00cc450178dc797b1138268355a4e83fc7a214f
BrandedNomad <info@brandednomad.com>
2022-02-24 15:46:56 +0700

    added comments

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,10 +46,24 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(
                     GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+        /**
+         * The setHsOptionsMenu method tells android that this fragment
+         * has an options menu, in which case it is going to call the
+         * onCreateOptionsMenu defined as a method below
+         */
         setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    /**
+     * @method getShareIntent
+     * @Description uses ShareCompat to create an intent
+     * that will share a message that contains. The ShareCompat api is
+     * specifically for creating intents that will share content like emails,
+     * messages, posts, images etc.
+     * @return The created intent
+     */
     private fun getShareIntent() : Intent {
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
         return ShareCompat.IntentBuilder.from(activity!!)
@@ -58,20 +72,50 @@ class GameWonFragment : Fragment() {
                 .intent
     }
 
+    /**
+     * @method shareSuccess
+     * @description uses the startActivity method to launh the
+     * intent created by the shareCompat api.
+     * @Return void
+     */
     private fun shareSuccess() {
         startActivity(getShareIntent())
     }
 
+    /**
+     * @method onCreateOptionsMenu
+     * @Description inflates the specified menu, in this case it is a
+     * social share button
+     * @param {Menu} menu
+     * @param {MenuInflater} inflater
+     *
+     */
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu, menu)
-        // check if the activity resolves
+
+        /**
+         * The resolveActivity method checks whether there are any
+         * apps that can actually handle the content to be shared
+         * If no apps are found, then it sets the visibility of the
+         * Share menu to false.
+         *
+         * It does so by checking with the packageManager which is aware of
+         * every activity that is registered in the android manifest of every
+         * application
+         */
         if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
             // hide the menu item if it doesn't resolve
             menu.findItem(R.id.share)?.isVisible = false
         }
     }
 
+    /**
+     * @method onOptionsItemSelected
+     * @description handles item selection on menu. When the share
+     * button is pressed it will call the shareSuccess method which
+     * will share the intent that has been created
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.share -> shareSuccess()
